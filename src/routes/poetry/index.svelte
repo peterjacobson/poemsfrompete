4f3f7db05188f689a20e6a7abf7e97a21e8bdc97
peterjacobson <peterjacobson.nz@gmail.com>
2021-05-14 20:28:48 +1200

    rename routes

--- src/routes/poetry/index.svelte
+++ src/routes/poetry/index.svelte
@@ -1,6 +1,6 @@
 <script context="module">
 	export function preload() {
-		return this.fetch(`poetry-home.json`).then(r => r.json()).then(poems => {
+		return this.fetch(`poetry.json`).then(r => r.json()).then(poems => {
 			return { poems };
 		});
 	}
@@ -25,5 +25,5 @@
       tell Sapper to load the data for the page as soon as
       the user hovers over the link or taps it, instead of
       waiting for the 'click' event -->
-  <p><a rel="prefetch" href="poetry-home/{poem.slug}">{poem.title}</a></p>
+  <p><a rel="prefetch" href="poetry/{poem.slug}">{poem.title}</a></p>
 {/each}
